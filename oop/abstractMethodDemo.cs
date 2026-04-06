4815d7e4645634c5badee65b1ef9017b3751147a
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-04-06 15:02:50 +0545

    abstract method in C#

---
+++ oop/abstractMethodDemo.cs
@@ -0,0 +1,34 @@
+using System;
+
+abstract class test1
+{
+    public int add (int i, int j)
+    {
+        return i + j;
+    }
+    public abstract int mul (int i, int j)
+    {
+        return i * j;
+            }
+}
+
+class test2 : test1
+{
+    public override int mul(int i, int j)
+    {
+        return i * j;
+    }
+}
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        test2 t = new test2();
+        int sum = t.add(10, 5);
+        Console.WriteLine("The sum is = " + sum);
+        int multiply = t.mul(5, 6);
+        Console.WriteLine($"The Multiplication is {multiply}");
+
+    }
+}
