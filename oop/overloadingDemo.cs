f53ae1b83941b14d68b4608243d3f9ca5913cff2
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-04-08 13:26:34 +0545

    operator overloading in C#

---
+++ oop/overloadingDemo.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Runtime.Intrinsics.Arm;
+
+class Complex
+{
+    private int x;
+    private int y;
+    public Complex()
+    {
+        
+    }
+
+    public Complex(int i, int j)
+    {
+        x = i;
+        y = j;
+    }
+
+    public void show()
+    {
+        Console.WriteLine("{0},{1}", x, y);
+    }
+
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        Complex temp = new Complex();
+        temp.x = c1.x + c2.x;
+        temp.y = c1.y + c2.y;
+        return temp;
+    }
+}
+
+class MyClass
+{
+    public static void Main()
+    {
+        Complex c1 = new Complex(20, 30);
+        c1.show();
+        Complex c2 = new Complex(30, 20);
+       Complex c3 = c1 + c2;
+        c3.show();
+    }
+}
