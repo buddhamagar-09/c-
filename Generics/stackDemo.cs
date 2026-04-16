138d5d89e2f5697d0ac1d99882d467701efca475
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-04-16 13:03:12 +0545

    stack in C#

---
+++ Generics/stackDemo.cs
@@ -0,0 +1,32 @@
+// Queue members
+// pop()
+// push()
+
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Stack<String> stack1 = new Stack<string>();
+        stack1.Push("BCA");
+        stack1.Push("MCA");
+        stack1.Push("CSIT");
+        stack1.Push("BBA");
+        stack1.Push("BIM");
+
+        Console.WriteLine("Elements in Stack are: ");
+        foreach (String s in stack1)
+        {
+            Console.WriteLine(s);
+        }
+
+        stack1.Pop();
+        stack1.Pop();
+        Console.WriteLine("After popping: ");
+        foreach (String item in stack1)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
