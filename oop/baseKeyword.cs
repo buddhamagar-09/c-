67297eec43c0a14d74eafb5aff17e5d6a5fe99dc
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-05-02 23:09:33 +0545

    Base keyword in C#

--- oop/baseKeyword.cs
+++ oop/baseKeyword.cs
@@ -20,3 +20,5 @@ public class Dog : Animal
         d.show();
     }
 }
+
+// base keyword is used to access the members of the base class from the derived class. In the above example, we have a base class called Animal which has a member variable color. The derived class Dog also has a member variable color. To access the color variable of the base class, we use the base keyword.
