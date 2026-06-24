a9f005c27fd847933e660e405fcdab6c1fee7361
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-24 22:05:39 +0545

    Name Space Example

--- basics/NameSpace.cs
+++ basics/NameSpace.cs
@@ -5,23 +5,28 @@ namespace outer
     {
         public void show()
         {
-            Console.WriteLine("Inside Program 1");
+            Console.WriteLine("i love Watching Anime.");
         }
     }
+
     namespace inner
     {
         class Program2
         {
             public void display()
             {
-                Program1 p = new Program1();
-                p.show();
-            } 
+                Program1 p1 = new Program1();
+                p1.show();
+            }
             static void Main()
             {
-                Program2 p2 = new Program2();
-                p2.display();
+                    Program2 p2= new Program2();
+                    p2.display();
             }
         }
     }
 }
+
+// namespace = a container that holds a set of identifiers and allows the organization of code into a hierarchical structure. 
+// It helps to avoid naming conflicts by providing a way to group related classes, interfaces, and other types together. 
+// In C#, namespaces are defined using the "namespace" keyword followed by the name of the namespace. They can be nested within each other to create a hierarchy of namespaces.
